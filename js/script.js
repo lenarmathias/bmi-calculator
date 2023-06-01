6f25bf53ae6fee595af3537890cdc50a5cf4fb68
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-06-01 03:20:42 +0200

    Added BMI category and fixed result element

--- js/script.js
+++ js/script.js
@@ -8,9 +8,32 @@
     const bmiResult = () => {
         const weight = document.querySelector(".js-weight").value;
         const height = document.querySelector(".js-height").value;
-        const result = document.querySelector(".js-resultText");
+        const resultText = document.querySelector(".js-resultText");
+        let result;
 
-        result.innerText = `${(weight / (height / 100)** 2).toFixed(1)}`
+        result = (weight / (height / 100) ** 2).toFixed(1)
+        resultText.innerText = `${result}`
+
+        bmiCategory(result);
+    };
+
+    const bmiCategory = (result) => {
+        const categoryResult = document.querySelector(".js-bmiCategory");
+
+        switch (true) {
+            case (result < 18.5):
+                categoryResult.innerText = "Underweight";
+                break;
+            case (result >= 18.5 && result <= 24.9):
+                categoryResult.innerText = "Normal weight";
+                break;
+            case (result >= 25 && result <= 29.9):
+                categoryResult.innerText = "Overweight";
+                break;
+            case (result >= 30):
+                categoryResult.innerText = "Obesity";
+                break;
+        }
     };
 
     const onFormSubmit = (event) => {
