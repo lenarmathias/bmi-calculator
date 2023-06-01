884c0ce979c1234abd2962fd582f1f95eb8a8fa7
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-06-01 02:00:21 +0200

    Implement basic BMI calculation function"

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 {
+    const bmiResult = () => {
+        let weight = document.querySelector(".js-weight").value;
+        let height = document.querySelector(".js-height").value;
+        const result = document.querySelector(".js-resultText");
+
+        result.innerText = `${(weight / (height / 100)** 2).toFixed(1)}`
+    };
+
     const onFormSubmit = (event) => {
         event.preventDefault();
+
+        bmiResult();
     };
 
     const init = () => {
-        const form = document.querySelector("js-form");
+        const form = document.querySelector(".js-form");
 
         form.addEventListener("submit", onFormSubmit);
     };
