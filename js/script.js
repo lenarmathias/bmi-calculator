2ace893f86849839039685778ac7acee931a8302
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-06-01 17:52:40 +0200

    Implemented BMI suggestion, optimized JS, HTML fix

--- js/script.js
+++ js/script.js
@@ -9,17 +9,18 @@
         const weight = document.querySelector(".js-weight").value;
         const height = document.querySelector(".js-height").value;
         const resultText = document.querySelector(".js-resultText");
+        const categoryResult = document.querySelector(".js-bmiCategory");
+
         let result;
 
         result = (weight / (height / 100) ** 2).toFixed(1)
         resultText.innerText = `${result}`
 
-        bmiCategory(result);
+        bmiCategory(result, categoryResult);
+        bmiSuggestion(height, categoryResult);
     };
 
-    const bmiCategory = (result) => {
-        const categoryResult = document.querySelector(".js-bmiCategory");
-
+    const bmiCategory = (result, categoryResult) => {
         switch (true) {
             case (result < 18.5):
                 categoryResult.innerText = "Underweight";
@@ -33,6 +34,18 @@
             case (result >= 30):
                 categoryResult.innerText = "Obesity";
                 break;
+        }
+    };
+
+    const bmiSuggestion = (height, categoryResult) => {
+        const bmiSuggestion = document.querySelector(".js-bmiSuggestion");
+
+        switch (categoryResult.innerText) {
+            case ("Normal weight"):
+                bmiSuggestion.innerText = "";
+                break;
+            default:
+                bmiSuggestion.innerText = `To achieve a normal weight at a height of ${height} cm, your weight should fall within the range of ${(18.5 * (height / 100) ** 2).toFixed(0)} kg to ${(24.9 * (height / 100) ** 2).toFixed(0)} kg based on the Body Mass Index (BMI).`
         }
     };
 
