c2ad8c3f76add0b8327c324d06d0a20c127826b7
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-06-01 23:58:26 +0200

    Implemented hidden sections and improved reset

--- js/script.js
+++ js/script.js
@@ -5,16 +5,32 @@
         firstInput.focus();
     };
 
+    const resetAll = () => {
+        const resultSection = document.querySelector(".js-resultSection");
+        const suggestionSection = document.querySelector(".js-suggestionSection");
+
+        if (!resultSection.classList.contains("section--hidden")) {
+            resultSection.classList.add("section--hidden");
+        }
+
+        if (!suggestionSection.classList.contains("section--hidden")) {
+            suggestionSection.classList.add("section--hidden");
+        }
+    };
+
     const bmiResult = () => {
         const weight = document.querySelector(".js-weight").value;
         const height = document.querySelector(".js-height").value;
         const resultText = document.querySelector(".js-resultText");
         const categoryResult = document.querySelector(".js-bmiCategory");
+        const resultSection = document.querySelector(".js-resultSection");
 
         let result;
 
         result = (weight / (height / 100) ** 2).toFixed(1)
         resultText.innerText = `${result}`
+
+        resultSection.classList.remove("section--hidden")
 
         bmiCategory(result, categoryResult);
         bmiSuggestion(height, categoryResult);
@@ -39,14 +55,23 @@
 
     const bmiSuggestion = (height, categoryResult) => {
         const bmiSuggestion = document.querySelector(".js-bmiSuggestion");
+        const suggestionSection = document.querySelector(".js-suggestionSection");
 
         switch (categoryResult.innerText) {
             case ("Normal weight"):
                 bmiSuggestion.innerText = "";
+                suggestionSection.classList.add("section--hidden");
                 break;
             default:
-                bmiSuggestion.innerText = `To achieve a normal weight at a height of ${height} cm, your weight should fall within the range of ${(18.5 * (height / 100) ** 2).toFixed(0)} kg to ${(24.9 * (height / 100) ** 2).toFixed(0)} kg based on the Body Mass Index (BMI).`
+                bmiSuggestion.innerText = `To achieve a normal weight at a height of ${height} cm, your weight should fall within the range of ${(18.5 * (height / 100) ** 2).toFixed(0)} kg to ${(24.9 * (height / 100) ** 2).toFixed(0)} kg based on the Body Mass Index (BMI).`;
+                suggestionSection.classList.remove("section--hidden");
         }
+    };
+
+    const bindEvents = () => {
+        const resetButton = document.querySelector(".js-resetButton");
+
+        resetButton.addEventListener("click", resetAll);
     };
 
     const onFormSubmit = (event) => {
@@ -61,6 +86,8 @@
         const form = document.querySelector(".js-form");
 
         form.addEventListener("submit", onFormSubmit);
+
+        bindEvents();
     };
 
     init();
